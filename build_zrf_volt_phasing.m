7c46c281e8b1df04d435ebfeeabc99e3cae2f93c
JackH <jack@rtr>
2024-01-16 16:05:20 +0000

    Add back in build commands to build script

--- build_zrf_volt_phasing.m
+++ build_zrf_volt_phasing.m
@@ -6,9 +6,9 @@ function build_zrf_volt_phasing(model_name, fpga_part, nof_chan_bits)
     %   nof_chan_bits : int : Set number of output channels. Number of output frequency channels = 2^nof_chan_bits
 
     model = generate_zrf_volt_phasing(model_name, fpga_part, nof_chan_bits);
-    % t0 = datetime;
-    % a = jasper_frontend(model); system([a ' --jobs 12'])
-    % t1 = datetime;
-    % build_duration = t1 - t0;
-    % sprintf('Build ended after duration %s', build_duration);
+    t0 = datetime;
+    a = jasper_frontend(model); system([a ' --jobs 12'])
+    t1 = datetime;
+    build_duration = t1 - t0;
+    sprintf('Build ended after duration %s', build_duration);
 end
